359633dc08d893102bfc8a9486e7f142eb625366
Carwyn Collinsworth <crcollinswor@cs.stonybrook.edu>
2021-10-31 20:37:19 -0400

    Added original Cart_Hill_Problem code. Note that this model translation to the drone model did not work

---
+++ 2D_Simulations/Cart_Hill_Problem/tfjs-mountaincar/src/js/memory.js
@@ -0,0 +1,30 @@
+export class Memory {
+    /**
+     * @param {number} maxMemory
+     */
+    constructor(maxMemory) {
+        this.maxMemory = maxMemory;
+        this.samples = new Array();
+    }
+
+    /**
+     * @param {Array} sample
+     */
+    addSample(sample) {
+        this.samples.push(sample);
+        if (this.samples.length > this.maxMemory) {
+            let [state,,, nextState] = this.samples.shift();
+            state.dispose();
+            nextState.dispose();
+        }
+    }
+
+    /**
+     * @param {number} nSamples
+     * @returns {Array} Randomly selected samples
+     */
+    sample(nSamples) {
+        // return sampleSize(this.samples, nSamples); // ORIGINAL STATEMENT
+        return this.samples.slice(-nSamples);
+    }
+}
